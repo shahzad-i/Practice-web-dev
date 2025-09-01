939888f31eb5349e6f99cc4114c44064c610ae98
shahzadibatool <shahzadibatool2008@gmail.com>
2025-09-01 04:47:19 -0700

    first commit

---
+++ give.js
@@ -0,0 +1,23 @@
+// export const detail={
+
+//     name:"ALi",
+// age:34,
+// school:"ABD school"
+// }
+
+// export function greet(){
+//     return `HI, ${detail.name} I am ${detail.age} years old.`
+// }
+
+
+ const detail={
+
+    name:"ALi",
+age:34,
+school:"ABD school"
+}
+
+ function greet(){
+    return `HI, ${detail.name} I am ${detail.age} years old.and i read in ${detail.school}`
+}
+export default greet
